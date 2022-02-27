6c6dc3430df26acbe3a7fdf93be77202024637fc
waq-e <whomomlikes@hotmail.com>
2022-02-26 19:42:40 -0600

    Initial commit.

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+class GradeBook {
+public:
+    void displayMessage() {
+        cout << "Welcome to the Grade Book!\n";
+    }
+};
+
+int main() {
+    GradeBook myGradeBook;
+    myGradeBook.displayMessage();
+
+    return 0;
+}
